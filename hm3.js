9615d3b03d392f515fa4e6363422f444f8418002
amirrezajalili <pouyagorge74@gmail.com>
2022-04-07 12:29:57 +0430

    first commit

---
+++ hm3.js
@@ -0,0 +1,22 @@
+let listekarmandan = [];
+
+function addMember(name, daramad, saatkari) {
+  let karmand = {name,daramad,saatkari,};
+    
+  listekarmandan.push(karmand);
+}
+
+addMember("jalili", 3500000, 240);
+console.log(listekarmandan);
+
+function removeMember(name) {
+  let index = listekarmandan.findIndex(function(iteam){ return iteam.name === name});
+  let user = null;
+  if (index >= 0) {
+    user = listekarmandan[index];
+    listekarmandan.splice(index, 1);
+  }
+  return user;
+}
+console.log(removeMember("jalili"));
+console.log(listekarmandan);
